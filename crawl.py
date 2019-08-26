358c5cb12eeef09c05f04013e6e73428b01e0769
Liu Miao <liumiao@bytedance.com>
2019-08-26 15:57:07 +0800

    a
    Change-Id: Ieea0628f43daba361e4a36f6adb51574c14d10b9

---
+++ crawl.py
@@ -0,0 +1,35 @@
+import sys
+import requests
+import urlparse
+
+def crawl_all():
+	for i in xrange(8):
+		url = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx={}&n=1&pid=hp'.format(i)
+		data = requests.get(url).json()
+		image_data = data['images'][0]
+		img_url = urlparse.urljoin(url, image_data['url'])
+		title = image_data['copyright']
+		date = image_data['startdate']
+		filename = 'images/{}.jpg'.format(date)
+		rsp = requests.get(img_url)
+		with open(filename, 'wb') as file:
+			file.write(rsp.content)
+		print 'save image to', filename
+
+
+def crawl_daily():
+	url = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&pid=hp'
+	data = requests.get(url).json()
+	image_data = data['images'][0]
+	img_url = urlparse.urljoin(url, image_data['url'])
+	title = image_data['copyright']
+	date = image_data['startdate']
+	filename = 'images/{}.jpg'.format(date)
+	rsp = requests.get(img_url)
+	with open(filename, 'wb') as file:
+		file.write(rsp.content)
+	print 'save image to', filename
+
+
+if __name__ == '__main__':
+	crawl_daily()
