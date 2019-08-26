91573f2db9f5cb84411a0cc8428fa55a33dcf637
Liu Miao <liumiao@bytedance.com>
2019-08-26 20:38:19 +0800

    a
    Change-Id: I223e1b1713613f718261b9013df04d7120b5f020

--- crawl.py
+++ crawl.py
@@ -3,14 +3,14 @@ import requests
 import urlparse
 
 def crawl_all():
-    for i in xrange(8):
+    for i in xrange(8, -1, -1):
         url = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx={}&n=1&pid=hp'.format(i)
         data = requests.get(url).json()
         image_data = data['images'][0]
         img_url = urlparse.urljoin(url, image_data['url'])
         title = image_data['copyright']
-        date = image_data['startdate']
-        filename = 'images/{}.jpg'.format(date)
+        filename = image_data['urlbase'].replace('/th?id=OHR.', '').split('_')[0]
+        filename = 'images/{}.jpg'.format(filename)
         rsp = requests.get(img_url)
         with open(filename, 'wb') as file:
             file.write(rsp.content)
@@ -23,8 +23,8 @@ def crawl_daily():
     image_data = data['images'][0]
     img_url = urlparse.urljoin(url, image_data['url'])
     title = image_data['copyright']
-    date = image_data['startdate']
-    filename = 'images/{}.jpg'.format(date)
+    filename = image_data['urlbase'].replace('/th?id=OHR.', '').split('_')[0]
+    filename = 'images/{}.jpg'.format(filename)
     rsp = requests.get(img_url)
     with open(filename, 'wb') as file:
         file.write(rsp.content)
@@ -34,10 +34,17 @@ def crawl_daily():
 def gen_html():
     import os
     import datetime
-    for parent, dirnames, filenames in os.walk('images'):
-        filenames = sorted(list(filenames), reverse=True)
-        for filename in filenames:
-            print '<div class="col-md-4"><img src="images/{}" class="img-responsive"></div>'.format(filename)
+    # for parent, dirnames, filenames in os.walk('images'):
+    #     filenames = sorted(list(filenames), reverse=True)
+    #     for filename in filenames:
+    #         print '<div class="col-md-4"><img src="images/{}" class="img-responsive"></div>'.format(filename)
+    dir_list = os.listdir('images')
+    if not dir_list:
+        return
+    else:
+        dir_list = sorted(dir_list, key=lambda x: os.path.getctime(os.path.join(os.path.split(os.path.realpath(__file__))[0] + '/images', x)))
+        for filename in dir_list:
+            print '<div class="col-md-4 col-sm-6 col-xs-12"><img src="data:image/gif;base64,R0lGODdhAQABAPAAAMPDwwAAACwAAAAAAQABAAACAkQBADs=" data-src="images/{}" class="img-responsive lazyload" width="1117" height="628"></div>'.format(filename)
 
 
 if __name__ == '__main__':
