b26ef2a8e591366442fdbc225830352f3d8e86c4
Liu Miao <liumiao@bytedance.com>
2019-08-27 11:53:27 +0800

    a
    Change-Id: I44c2eacce37a0ba08f2425d17ce21411fa02a945

--- crawl.py
+++ crawl.py
@@ -65,7 +65,7 @@ def gen_html():
     for filename in dir_list:
         if not filename.endswith('.jpg'):
             continue
-        repl += '<div class="col-md-4 col-sm-6 col-xs-12"><img src="data:image/gif;base64,R0lGODdhAQABAPAAAMPDwwAAACwAAAAAAQABAAACAkQBADs=" data-src="images/{}" class="img-responsive lazyload" width="1117" height="628"></div>\n'.format(filename)
+        repl += '<div class="col-md-4 col-sm-6 col-xs-12"><img src="data:image/gif;base64,R0lGODdhAQABAPAAAMPDwwAAACwAAAAAAQABAAACAkQBADs=" data-src="images/{}" class="img-responsive lazyload" width="1920" height="1080"></div>\n'.format(filename)
     html = html_tpl % repl
     with open('index.html', 'w') as f:
         f.write(html)
